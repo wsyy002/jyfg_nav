bdc6eab35aa29510b7378a21c1b139abb8b6d8a4
wsyy002 <107821418+wsyy002@users.noreply.github.com>
2026-03-10 12:00:56 +0800

    chore: 更新导航数据 - 2026/3/10 12:00:56

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -481,7 +481,7 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "_fileSha": "ea3f00cba99e7d9e1d5cb7b816f6a7836b8c233e",
+  "title": "我的导航🐱",
+  "_fileSha": "061661f05b7aa741907ec428cad9a0af57da8d5a",
   "search": "baidu"
 }
